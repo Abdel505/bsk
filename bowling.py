76551c235b219275aa94090066e1ba88b2a5fdaa
Abdel505 <abdelalouan07@gmail.com>
2025-11-11 11:59:17 +0100

    test add and get frame @user_story_1

--- bowling.py
+++ bowling.py
@@ -5,13 +5,20 @@ from frame import Frame
 class BowlingGame:
 
     def __init__(self):
-        pass
-    
+        
+        self._frames = []
+        self._MAX_FRAMES = 10
+
     def add_frame(self, frame: Frame) -> None:
-        pass
+        if len(self._frames) < self._MAX_FRAMES:
+            self._frames.append(frame)
+        else:
+            raise BowlingError("A game can only have 10 frames.")
 
     def get_frame_at(self, i: int) -> Frame:
-        pass
+        if 0 <= i < len(self._frames):
+            return self._frames[i]
+        raise BowlingError("Frame index out of bounds.")
 
     def calculate_score(self) -> int:
         pass
--- test/test_bowling.py
+++ test/test_bowling.py
@@ -1,4 +1,7 @@
 import unittest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from bowling import BowlingGame
 from bowling_error import BowlingError
@@ -7,5 +10,17 @@ from frame import Frame
 
 class TestBowlingGame(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_add_and_get_frame(self):
+        game = BowlingGame()
+        frame = Frame(1, 5)
+        game.add_frame(frame)
+        self.assertEqual(len(game._frames), 1)
+        self.assertEqual(game.get_frame_at(0), frame)
+
+
+    def test_get_first_and_second_throw(self):
+        game = BowlingGame()
+        frame = Frame(1, 5)
+        game.add_frame(frame)
+        self.assertEqual(game.get_frame_at(0).get_first_throw(), 1)
+        self.assertEqual(game.get_frame_at(0).get_second_throw(), 5)
